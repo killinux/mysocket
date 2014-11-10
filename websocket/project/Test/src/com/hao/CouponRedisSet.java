2fc3c8d4e6a233037b314acfc7295ffd63f985e4
haoning <haoning@fun.tv>
2014-11-10 21:44:20 +0800

    coupon

--- websocket/project/Test/src/com/hao/CouponRedisSet.java
+++ websocket/project/Test/src/com/hao/CouponRedisSet.java
@@ -1,14 +1,18 @@
 package com.hao;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.CachedRowSet;
 
 import redis.clients.jedis.Jedis;
+
+import com.efan.dao.DBEngine;
 
 public class CouponRedisSet  extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -18,10 +22,14 @@ public class CouponRedisSet  extends HttpServlet {
 		System.out.println("---------/coupon/check");
 		try {   
 			String machine_product_id = request.getParameter("machine_product_id"); 
+			String user_id = request.getParameter("user_id"); 
+			String channel_id = request.getParameter("channel_id"); 
+			
 			String result =null;
 			if(machine_product_id==null){
 				 result="0";
 			}else{
+				 //修改数据库 -1 操作
 				 result=jedis.set(machine_product_id,"1"); 
 				 System.out.println(result);
 			}
@@ -38,7 +46,24 @@ public class CouponRedisSet  extends HttpServlet {
 			
 		}  
 	}
-
+	private int setCoupon(String user_id,String channel_id){
+		int result=-1;
+		try{
+			DBEngine dbe = new DBEngine("efan",false);
+			String sql="select ott_app.*,ott_channel_app_rel.app_weight " +
+					"from ott_app ,ott_channel_app_rel " +
+					"where " +
+					" ott_app.pub_status = 'P' " +
+					"  and ott_app.id = ott_channel_app_rel.app_id" +
+					" and ott_channel_app_rel.channel_id = '"+user_id+"'";
+			result = dbe.executeUpdate(sql);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return result;
+	}
 	// 用于处理客户端发送的POST请求
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
---
+++ websocket/project/Test/src/com/qq/jutil/sql/Test.java
@@ -0,0 +1,5 @@
+package com.qq.jutil.sql;
+
+public class Test {
+
+}
